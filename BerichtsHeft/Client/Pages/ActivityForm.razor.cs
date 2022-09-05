6694dcf7f102dc24463731501c3b8a2ebfcc9fe4
bailin.zheng <bailin.zheng@SEBASTIAN-NB>
2022-09-05 15:02:13 +0200

    Add project files.

---
+++ BerichtsHeft/Client/Pages/ActivityForm.razor.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.JSInterop;
+namespace BerichtsHeft.Client.Pages
+{
+	//public class ActivityForm
+	//{
+ //       public Activity datei = new Activity();
+
+ //       public void activityanmelden()
+ //       {
+ //           Activity.Activities.Add(datei);
+ //           NavigationManager.NavigateTo("dateitresor");
+ //       }
+
+ //       void HandleReport()
+ //       {
+ //           JSRuntime.InvokeVoidAsync("console.log", "Update is done!", datei);
+ //       }
+ //   }
+}
---
+++ BerichtsHeft/Shared/DateiInfo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BerichtsHeft.Shared
+{
+    public class DateiInfo
+    {   
+        public static List<DateiInfo> Activities { get; private set; } = new List<DateiInfo>();
+        public DateTime DateOfReport { get; set; } = DateTime.Now;
+        [Range(typeof(bool), "true", "true", ErrorMessage = "Only confirmed users can play!")]
+        public string AbgabeType { get; set; }
+        public string HauptText { get; set; }
+        public int DateBlock { get; set; }
+        public int Dauertmin { get; set; }
+        public string WochenTag { get; set; }
+        public string Name { get; set; }
+        public string Subject { get; set; }
+
+    }
+}
