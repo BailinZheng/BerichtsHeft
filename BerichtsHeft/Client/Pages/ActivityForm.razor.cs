eaf3670d48157784633b70ed5cd3d3c04c61a701
bailin.zheng <bailin.zheng@SEBASTIAN-NB>
2022-09-05 16:53:59 +0200

    veränden

--- BerichtsHeft/Client/Pages/ActivityForm.razor.cs
+++ BerichtsHeft/Client/Pages/ActivityForm.razor.cs
@@ -1,19 +1,32 @@
-﻿using Microsoft.JSInterop;
+﻿using BerichtsHeft.Shared;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
 namespace BerichtsHeft.Client.Pages
 {
-	//public class ActivityForm
-	//{
- //       public Activity datei = new Activity();
+    public class ActivityFormClass : ComponentBase
+    {
+        [Inject]
+        public NavigationManager NavMan { get; set; }
+        [Parameter]
+        public string ActivityIndex { get; set; }
+        public DateiInfo Datei = new DateiInfo();
 
- //       public void activityanmelden()
- //       {
- //           Activity.Activities.Add(datei);
- //           NavigationManager.NavigateTo("dateitresor");
- //       }
+        public void activityanmelden()
+        {
+            DateiInfo.Activities.Add(Datei);
+            NavMan.NavigateTo("dateitresor");
+        }
+        protected override void OnInitialized()
+        {
+            //ActivityIndex = ActivityIndex ?? null;
 
- //       void HandleReport()
- //       {
- //           JSRuntime.InvokeVoidAsync("console.log", "Update is done!", datei);
- //       }
- //   }
+            if (ActivityIndex != null)
+            {
+                int index = Convert.ToInt32(ActivityIndex);
+                int count = DateiInfo.Activities.Count;
+                if(Activities>= count)
+                Datei = DateiInfo.Activities[index];
+            }
+        }
+    }
 }
