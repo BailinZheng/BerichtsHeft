b185e1fb9f3f8035588dd6bfb533933789f783eb
andrileanu <78201907+andrileanu@users.noreply.github.com>
2022-09-07 11:47:58 +0200

    Add files via upload
    data

---
+++ Data/Repository/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using BerichtsHeft.Shared;
+
+namespace BerichtsHeft.Client.Data.Repository
+{
+    public interface IRepository
+    {
+        IEnumerable<DateiInfo> CRUD_Create();
+
+        void CRUD_Read(string dateiName);
+
+        void CRUD_Edit(DateiInfo changedItem);
+
+        void CRUD_Delete(int id);
+    }
+}
---
+++ Data/Repository/SQLRepository.cs
@@ -0,0 +1,51 @@
+﻿using BerichtsHeft.Shared;
+
+namespace BerichtsHeft.Client.Data.Repository
+{
+    public class SQLRepository : IRepository
+    {
+        private readonly DB _context;
+
+        public SQLRepository(DB context)
+        {
+            _context = context;
+        }
+        public IEnumerable<DateiInfo> CRUD_Create()
+        {
+            return _context.DateiInfos;
+        }
+
+        public IEnumerable<DateiInfo> CRUD
+
+        public void CRUD_Delete(int id)
+        {
+            var deletedItem = _context.DateiInfos.Find(id);
+
+            if (deletedItem != null)
+            {
+                _context.DateiInfos.Remove(deletedItem);
+                _context.SaveChanges();
+            }
+        }
+
+        public void CRUD_Edit(DateiInfo changedItem)
+        {
+            var item = _context.DateiInfos.Attach(changedItem);
+            item.State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+
+            _context.SaveChanges();
+        }
+
+        public void CRUD_Read(string dateiName)
+        {
+            DateiInfo newItem = new DateiInfo()
+            {
+                HauptText = dateiName
+
+            };
+
+            _context.DateiInfos.Add(newItem); 
+            _context.SaveChanges();
+        }
+    }
+}
